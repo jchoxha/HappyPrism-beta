9a9113d1f9c8e86dde534a18af821c84851110b9
Jeton Hoxha <jeton_hoxha@student.uml.edu>
2024-07-09 03:01:16 +0300

    Set up production build and static file serving

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-// server.js
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const _dirname = __dirname;
 
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(_dirname, 'public', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
